32a965e3ca238fd5ef6b17452f036d683dc8308e
sammy-code98 <sammysmart98@yahoo.com>
2025-01-16 13:38:40 +0100

    feat: setup navigation and add tabs

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -12,10 +12,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
   },
   text: {
-    color: '#fff'
-  }
-})
+    color: '#fff',
+  },
+});
